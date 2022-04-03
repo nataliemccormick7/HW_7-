ed3c5ca3ada0f9d0e2a556bd9699b3e474d406cf
Natalie McCormick <72220179+nataliemccormick7@users.noreply.github.com>
2022-04-03 16:02:08 -0500

    comitt

--- app.py
+++ app.py
@@ -13,20 +13,8 @@ def index():
 def about():
     return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
 
-@app.route('/estimate', methods=['GET''POST'])
+@app.route('/estimate')
 def estimate():
-total_estimate= " "
-    if request.method=="POST":
-        radius= request.form['tank_weight']
-        height= request.form['tank_height']
-        area_tank_top= pi* radius^2
-        area_sides=2*(pi(radius*height))
-        total_area= area_tank_top + area_sides
-        square_feet= total_area/144
-        material_cost= square_feet*25
-        labor_cost= square_feet*15
-        total_estimate= material_cost+ labor_cost
-        return redirect(url_for('estimate'))
     return render_template('estimate.html', pageTitle='Make Estimate')
 
 
