1db9b117477805636a51572abc741261ab857e46
Natalie McCormick <72220179+nataliemccormick7@users.noreply.github.com>
2022-04-03 15:17:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask
-from flask import render_template
+from cmath import pi
+from flask import Flask, redirect
+from flask import render_template, redirect, request, url_for
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance ')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
+
+@app.route('/estimate', methods=['GET''POST'])
+def estimate():
+total_estimate= " "
+    if request.method=="POST":
+        radius= request.form['tank_weight']
+        height= request.form['tank_height']
+        area_tank_top= pi* radius^2
+        area_sides=2*(pi(radius*height))
+        total_area= area_tank_top + area_sides
+        square_feet= total_area/144
+        material_cost= square_feet*25
+        labor_cost= square_feet*15
+        total_estimate= material_cost+ labor_cost
+        return redirect(url_for('estimate'))
+    return render_template('estimate.html', pageTitle='Make Estimate')
+
+
+   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
